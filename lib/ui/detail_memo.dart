c29d2170db83903cb57be5f69eaa9e03af74aa73
seil.chu <seil.chu@sk.com>
2023-01-11 18:25:30 +0900

    mobile - desktop : responsive ui

--- lib/ui/detail_memo.dart
+++ lib/ui/detail_memo.dart
@@ -4,31 +4,52 @@ import '../data/memo_data.dart';
 
 class DetailMemo extends StatelessWidget {
   final MemoData memoData;
+  final bool showAppbar;
 
-  const DetailMemo({required this.memoData, super.key});
+  const DetailMemo({
+    required this.memoData,
+    required this.showAppbar,
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) {
-    return
-      Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            const SizedBox(height: 5,),
-            Text(
-              memoData.title.toString(),
-              style: const TextStyle(color: Colors.black, fontSize: 20),
-            ),
-            const SizedBox(height: 5,),
-            Expanded(
-              child: Text(
-                memoData.content.toString(),
-                style: const TextStyle(color: Colors.grey, fontSize: 14),
-              ),
-            ),
-          ],
+    if (showAppbar) {
+      return Scaffold(
+        appBar: AppBar(
+          shadowColor: Colors.transparent,
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          // title: Text(memoData.title.toString()),
         ),
+        body:_detailWidget(context),
       );
+    }
+    else {
+      return _detailWidget(context);
+    }
   }
 
+  Widget _detailWidget(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      color: Colors.white,
+      child: Column(
+        children: [
+          const SizedBox(height: 5,),
+          Text(
+            memoData.title.toString(),
+            style: const TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          const SizedBox(height: 5,),
+          Expanded(
+            child: Text(
+              memoData.content.toString(),
+              style: const TextStyle(color: Colors.grey, fontSize: 14),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/ui/page.dart
+++ lib/ui/page.dart
@@ -7,7 +7,7 @@ import 'listview_memo.dart';
 
 // ignore: must_be_immutable
 class AppPage extends StatefulWidget {
-  List<MemoData> dataList = MemoDatabase.dataList;
+  final List<MemoData> dataList = MemoDatabase.dataList;
 
   AppPage({super.key});
 
@@ -16,32 +16,61 @@ class AppPage extends StatefulWidget {
 }
 
 class AppPageState extends State<AppPage> {
+  static const int kMobileMaxWidth = 600;
   int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        // list drawer
-        SizedBox(
-          width: 300,
-          child: ListViewMemo(
-            dataList:widget.dataList,
-            selectedIndex: selectedIndex,
-            selectedCallback: (index) {
-              setState(() {
-                selectedIndex = index;
-              });
-            },
+    final bool mobileUi = MediaQuery.of(context).size.width < kMobileMaxWidth;
+    if (mobileUi) {
+      return ListViewMemo(
+        dataList: widget.dataList,
+        selectedIndex: selectedIndex,
+        selectedCallback: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+
+          Navigator.push(context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return DetailMemo(
+                    memoData: widget.dataList[selectedIndex],
+                    showAppbar: true,
+                  );
+                },
+              )
+          );
+        },
+      );
+    }
+    else {
+      return Row(
+        children: [
+          // list drawer
+          SizedBox(
+            width: 300,
+            child: ListViewMemo(
+              dataList: widget.dataList,
+              selectedIndex: selectedIndex,
+              selectedCallback: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
           ),
-        ),
-        const VerticalDivider(width: 0,),
-        // detail
-        Expanded(
-          child: DetailMemo(memoData:widget.dataList[selectedIndex]),
-        ),
-      ],
-    );
+          const VerticalDivider(width: 0,),
+          // detail
+          Expanded(
+            child: DetailMemo(
+              memoData: widget.dataList[selectedIndex],
+              showAppbar: false,
+            ),
+          ),
+        ],
+      );
+    }
   }
 
 }
