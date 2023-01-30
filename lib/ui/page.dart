162b4b9eda1bcf43c61189a53a45a5968c8e0b50
seil.chu <seil.chu@sk.com>
2023-01-30 13:53:02 +0900

    orientationBuilder 사용해서 반응형 ui 적용
    mediaQuery 를 바로 build() 에서 사용하지 않고 orientationBuilder 로 감싸서 사용

--- lib/ui/page.dart
+++ lib/ui/page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../data/memo_data.dart';
 import '../data/memo_database.dart';
 import '../provider/mobile_layout.dart';
 import '../provider/selected_memo.dart';
@@ -9,41 +8,63 @@ import 'detail_memo.dart';
 import 'listview_memo.dart';
 
 // ignore: must_be_immutable
-class AppPage extends StatelessWidget {
-  final List<MemoData> dataList = MemoDatabase.dataList;
+class AppPage extends StatefulWidget {
+  const AppPage({super.key});
+
+  @override
+  AppPageState createState() => AppPageState();
+}
+
+class AppPageState extends State<AppPage> {
   static const int kMobileMaxWidth = 400;
 
-  AppPage({super.key});
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Widget mobileWidget() {
+    return ListViewMemo(dataList: MemoDatabase.dataList);
+  }
+
+  Widget desktopWidget() {
+    return Row(
+      children: [
+        // list drawer
+        SizedBox(
+          width: 300,
+          child: ListViewMemo(dataList: MemoDatabase.dataList),
+        ),
+        const VerticalDivider(width: 0),
+        // detail
+        Expanded(
+          child: DetailMemo(
+            key: ValueKey(context.watch<SelectedMemo>().selectedMemo.id),
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final bool mobileUi = MediaQuery.of(context).size.width < kMobileMaxWidth;
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      // 첫빌드 이후에 실행
-      context.read<MobileLayout>().updateMobileLayout(mobileUi);
-    });
+    print(" AppPageState :: build");
 
-    if (mobileUi) {
-      return ListViewMemo(dataList: dataList);
-    }
-    else {
-      return Row(
-        children: [
-          // list drawer
-          SizedBox(
-            width: 300,
-            child: ListViewMemo(dataList: dataList),
-          ),
-          const VerticalDivider(width: 0),
-          // detail
-          Expanded(
-            child: DetailMemo(
-              key: ValueKey(context.watch<SelectedMemo>().selectedMemo.id),
-            ),
-          ),
-        ],
-      );
-    }
+    return Scaffold(
+      body: OrientationBuilder(builder: (context, orientation) {
+        final bool mobileUi = MediaQuery.of(context).size.width < kMobileMaxWidth;
+        WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+          context.read<MobileLayout>().updateMobileLayout(mobileUi);
+        });
+        return mobileUi? mobileWidget() : desktopWidget();
+      }),
+    );
   }
 
 }
