35ee35c5c4c148a69aabc668a7b632224c9b2806
seil.chu <seil.chu@sk.com>
2023-01-31 15:49:28 +0900

    init realm

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import realm
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  RealmPlugin.register(with: registry.registrar(forPlugin: "RealmPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
